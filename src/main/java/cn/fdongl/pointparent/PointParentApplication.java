e5c1101cfbacdfcabb9467f49f3645068900adbf
LiuFangdong <894856599@qq.com>
2019-08-28 13:49:52 +0800

    Initial commit

---
+++ src/main/java/cn/fdongl/pointparent/PointParentApplication.java
@@ -0,0 +1,13 @@
+package cn.fdongl.pointparent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PointParentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PointParentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cn/fdongl/pointparent/PointParentApplicationTests.java
@@ -0,0 +1,16 @@
+package cn.fdongl.pointparent;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PointParentApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
