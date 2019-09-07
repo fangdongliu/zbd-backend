aaafc026541774f90b943467e68dd48724465424
zm <zhoumiao48@gmail.com>
2019-09-07 15:36:41 +0800

    教师信息上传-coding

--- point/src/main/java/cn/fdongl/point/service/impl/UploadFrameServiceImpl.java
+++ point/src/main/java/cn/fdongl/point/service/impl/UploadFrameServiceImpl.java
@@ -26,6 +26,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.Iterator;
